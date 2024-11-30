3975b084a386467fca98c56300e7115484ad285b
KaileyCozart <kaileycozart@gmail.com>
2024-11-29 23:08:17 -0800

    VERY Important Script Update

--- Behavioral Analysis/B-SOiD/Scripts/Collect Groups for Analysis (Recommended).py
+++ Behavioral Analysis/B-SOiD/Scripts/Collect Groups for Analysis (Recommended).py
@@ -2,22 +2,32 @@
 import os
 import shutil
 
-# Collect the Group Examples from All Subdirectories
+# Collect Groups
 def organize_gif_files(base_dir):
+    gif_files = []
     for root, _, files in os.walk(base_dir):
         for file in files:
             if file.endswith(".gif"):
-                parts = file.split('_')
-                group = '_'.join(parts[:2]) 
-                group_folder_path = os.path.join(base_dir, group)
-                os.makedirs(group_folder_path, exist_ok=True)
-                src_file_path = os.path.join(root, file)
-                dest_file_path = os.path.join(group_folder_path, file)
-                shutil.move(src_file_path, dest_file_path)
-                print(f"Moved {file} to {group_folder_path}")
+                gif_files.append(os.path.join(root, file))
+    for gif_file in gif_files:
+        file_name = os.path.basename(gif_file)
+        parts = file_name.split('_')
+        group = '_'.join(parts[:2]) 
+        if not group.startswith("group_"):
+            print(f"Skipping file without group information: {file_name}")
+            continue
+        group_folder = os.path.join(base_dir, group)
+        os.makedirs(group_folder, exist_ok=True)
+        counter = 0
+        dest_file_path = os.path.join(group_folder, f"{group}_{counter}.gif")
+        while os.path.exists(dest_file_path):
+            counter += 1
+            dest_file_path = os.path.join(group_folder, f"{group}_{counter}.gif")
+        shutil.copy(gif_file, dest_file_path)
+        print(f"Copied {gif_file} to {dest_file_path}")
 
 # Run Function
-base_directory = r"C:\Users\Username\Path\to\Directory"
+base_directory = r"C:\Users\Username\Path\to\File\Bsoid\control\mp4s"
 organize_gif_files(base_directory)
 
 # Source: ChatGPT
